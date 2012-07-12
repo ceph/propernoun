fa32a4b9e4cea66ec89e3892bd40065d5da6020f
Tommi Virtanen <tv@inktank.com>
2012-07-12 10:22:15 -0700

    Wire events up to dns updater.
    This completes the circuit, and the app is now functional.

--- propernoun/main.py
+++ propernoun/main.py
@@ -1,14 +1,40 @@
 import Queue
+import logging
 
+import sqlalchemy as sq
+
+from . import collect
+from . import dns
 from . import leases
 from . import safethread
 from . import virt_mon
+
+
+log = logging.getLogger(__name__)
 
 
 def main(
     config,
     ):
     events = Queue.Queue()
+
+    engine = sq.create_engine(
+        config['pdns']['database'],
+        )
+    meta = sq.MetaData()
+    meta.bind = engine
+    meta.reflect()
+
+    dns_updater = dns.update(
+        meta=meta,
+        config=config,
+        )
+    # start the coroutine
+    dns_updater.next()
+
+    collector = collect.Collector(
+        callback=dns_updater.send,
+        )
 
     callables = []
 
@@ -33,11 +59,12 @@ def main(
             events.put(msg)
     callables.append(monitor_dhcp)
 
-    def collector():
+    def send_to_collector():
         """collector"""
         while True:
             msg = events.get()
-            print msg
-    callables.append(collector)
+            log.debug('Event: %r', msg)
+            collector.event(msg)
+    callables.append(send_to_collector)
 
     safethread.run_safe_threads(*callables)
